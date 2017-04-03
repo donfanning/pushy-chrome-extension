ef2a7cf5b3f1979e064463be9e653ed8d4e5d967
opus1269 <igreilly1269@gmail.com>
2017-04-03 17:57:05 -0500

    add global variable to make sure we are only injected once

--- app/scripts/on_copy_cut_content_script.js
+++ app/scripts/on_copy_cut_content_script.js
@@ -1,20 +1,28 @@
-'use strict';
+(function() {
+	'use strict';
 
-/**
- * Content script to monitor clipboard copy and cuts
- * @namespace ContentScript
- */
+	/**
+	 * Content script to monitor clipboard copy and cuts
+	 * @namespace ContentScript
+	 */
 
-/**
- * On copy or cut event, send a message to our extension
- * @memberOf ContentScript
- */
-function onMyCopy() {
-	chrome.runtime.sendMessage(chrome.runtime.id, {
-		message: 'copiedToClipboard',
-	}, (response) => {});
-}
+	if (window.injected) {
+		// every other attempt to inject but first
+		return;
+	}
+	window.injected = true;
 
-// register event listener for copy and cut events on document
-document.addEventListener('copy', onMyCopy, true);
-document.addEventListener('cut', onMyCopy, true);
+	/**
+	 * On copy or cut event, send a message to our extension
+	 * @memberOf ContentScript
+	 */
+	function onMyCopy() {
+		chrome.runtime.sendMessage(chrome.runtime.id, {
+			message: 'copiedToClipboard',
+		}, (response) => {});
+	}
+
+	// register event listeners for copy and cut events on document
+	document.addEventListener('copy', onMyCopy, true);
+	document.addEventListener('cut', onMyCopy, true);
+})();
