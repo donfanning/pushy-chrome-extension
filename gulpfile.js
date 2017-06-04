aa9f7a197cb8b61a654de70b27df6d821b1b774f
opus1269 <igreilly1269@gmail.com>
2017-06-04 04:23:01 -0500

    add copyright

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2016-2017, Michael A. Updike All rights reserved.
+ * Licensed under Apache 2.0
+ * https://opensource.org/licenses/Apache-2.0
+ * https://goo.gl/wFvBM1
+ */
 'use strict';
 
 // paths and files
